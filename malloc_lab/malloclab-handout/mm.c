5af574e4a47b93a02b0b96bcf3e9d41d4fe826dd
zhaorc <zhaorc@DESKTOP-SNCK4QB>
2025-05-27 05:31:45 +0800

    change lab team info

--- malloc_lab/malloclab-handout/mm.c
+++ malloc_lab/malloclab-handout/mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "ZRC",
     /* First member's full name */
-    "Harry Bovik",
+    "RongCheng Zhao",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "zhaorc@test.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
